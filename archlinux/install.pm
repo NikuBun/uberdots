6597f0c99809c61c711071056c62e33b80fdcf2b
Zak "Ubercow" Kristjanson <zak.kristjanson@gmail.com>
2013-11-01 23:12:18 -0700

    Changed linux module to arch linux specific

---
+++ archlinux/install.pm
@@ -0,0 +1,20 @@
+use File::Path qw(make_path);
+print BOLD BLUE, "[Arch Linux Modules]\n";
+print BOLD BLUE, "Common config for Arch Linux including awesomewm, fontconfig and lilyterm.\n";
+
+# Create .config if it doesn't exist
+print BLUE, "Checking directory existance, creating non-existing.\n", RESET;
+make_path($homedir . "/.config/fuckyou", {verbose => 1});
+
+# Symbolic Links
+sym("linux/awesome",    ".config/awesome");
+sym("linux/fontconfig", ".config/fontconfig");
+sym("linux/lilyterm",   ".config/lilyterm");
+sym("linux/gtk-3.0",    ".config/gtk-3.0");
+sym("linux/fonts",      ".fonts");
+
+print YELLOW, "To ensure the full experience, please install the following packages:\n", RESET
+print "Pacman: cantarell-fonts\n"
+print "AUR: gtk-theme-numix icons-theme-numix\n" 
+
+1;
