9cfe66080542b224a18f448062136b0063a4c7ef
Zak "Ubercow" Kristjanson <zak.kristjanson@gmail.com>
2013-11-02 23:19:21 -0700

    Bugfixed, style changed to be more readable, new global module and global mpv config

--- archlinux/install.pm
+++ archlinux/install.pm
@@ -1,6 +1,5 @@
 use File::Path qw(make_path);
-print BOLD BLUE, "[Arch Linux Modules]\n";
-print BOLD BLUE, "Common config for Arch Linux including awesomewm, fontconfig and lilyterm.\n";
+print BOLD YELLOW, "[Arch Linux Modules] Common config for Arch Linux including awesomewm, fontconfig and lilyterm.\n";
 
 # Create .config if it doesn't exist
 print BLUE, "Checking directory existance, creating non-existing.\n", RESET;
@@ -13,8 +12,8 @@ sym("linux/lilyterm",   ".config/lilyterm");
 sym("linux/gtk-3.0",    ".config/gtk-3.0");
 sym("linux/fonts",      ".fonts");
 
-print YELLOW, "To ensure the full experience, please install the following packages:\n", RESET
-print "Pacman: cantarell-fonts\n"
-print "AUR: gtk-theme-numix icons-theme-numix\n" 
-
+print YELLOW, "To ensure the full experience, please install the following packages:\n", RESET;
+print "Pacman: cantarell-fonts\n";
+print "AUR: gtk-theme-numix icons-theme-numix\n";
+print "\n\n";
 1;
---
+++ global/install.pm
@@ -0,0 +1,15 @@
+use File::Path qw(make_path);
+print BOLD YELLOW, "[Global Module]: Misc files that should be installed on anything.\n";
+
+# Create .config if it doesn't exist
+#print BLUE, "Checking directory existance, creating non-existing.\n", RESET;
+#make_path($homedir . "/.mpv", {verbose => 1});
+
+# Symbolic Links
+sym("global/mpv",    ".mpv");
+
+print YELLOW, "To ensure the full experience, please install the following packages:\n", RESET;
+print "Pacman: mpv\n";
+print "Homebrew: mpv\n";
+print "\n\n";
+1;
