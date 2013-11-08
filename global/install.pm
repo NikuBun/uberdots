54790a909494e3d1c5cb5e5abc4f5ce22a31394a
Zak "Ubercow" Kristjanson <ubercow1337@gmail.com>
2013-11-08 01:39:23 -0800

    Added debugging support for a fake homedir. Also removed some old code that I accidentally added back in.

--- global/install.pm
+++ global/install.pm
@@ -1,10 +1,6 @@
 use File::Path qw(make_path);
 header("Global", "Misc files that should be installed on anything.");
 
-# Create .config if it doesn't exist
-print BLUE, "Checking directory existance, creating non-existing.\n", RESET;
-make_path($homedir . "/.mpv", {verbose => 1});
-
 # Symbolic Links
 sym("global/mpv",    ".mpv");
 
